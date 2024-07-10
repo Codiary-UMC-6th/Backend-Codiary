1969b6b12b6c1719e07906c777fce492bb637356
yumzen <yeom.ji.eun35@gmail.com>
2024-07-11 01:16:06 +0900

    #1 Chore: Swagger 세팅

---
+++ src/main/java/com/codiary/backend/global/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.codiary.backend.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Codiary Swagger")
+                .description("Codiary swagger")
+                .version("1.0.0");
+    }
+}
