204a9bad0356a3ab6d0759ece842d4cdad479a55
insa658723 <insa658723@gmail.com>
2024-07-20 20:55:41 +0900

    #8 Feat: Comment 엔티티 생성

--- src/main/java/com/codiary/backend/global/domain/entity/Comment.java
+++ src/main/java/com/codiary/backend/global/domain/entity/Comment.java
@@ -1,4 +1,22 @@
 package com.codiary.backend.global.domain.entity;
 
-public class Comment {
+import com.codiary.backend.global.domain.common.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Comment extends BaseEntity {
+
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long commentId;
+
+  private String commentBody;
+
 }
