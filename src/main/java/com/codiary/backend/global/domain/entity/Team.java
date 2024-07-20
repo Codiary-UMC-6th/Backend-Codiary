de670ea1dd401fb916df33e6363936bdd8023e00
insa658723 <insa658723@gmail.com>
2024-07-20 20:30:23 +0900

    #8 Feat: Team 엔티티 생성

--- src/main/java/com/codiary/backend/global/domain/entity/Team.java
+++ src/main/java/com/codiary/backend/global/domain/entity/Team.java
@@ -1,4 +1,31 @@
 package com.codiary.backend.global.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Team {
+
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "team_id")
+  private Long teamId;
+
+  private String name;
+
+  private String intro;
+
+  private String profilePhoto;
+
+  private String github;
+
+  private String email;
+
+  private String linkedin;
+
+  private String instagram;
+
 }
