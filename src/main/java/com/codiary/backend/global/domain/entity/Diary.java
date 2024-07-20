f64edc04acec8a5c748bfc48c0a0c4d9fe2ad0e6
insa658723 <insa658723@gmail.com>
2024-07-20 23:55:45 +0900

    #8 Feat: Diary 엔티티 생성

--- src/main/java/com/codiary/backend/global/domain/entity/Diary.java
+++ src/main/java/com/codiary/backend/global/domain/entity/Diary.java
@@ -1,4 +1,33 @@
 package com.codiary.backend.global.domain.entity;
 
-public class Diary {
+import com.codiary.backend.global.domain.common.BaseEntity;
+import com.codiary.backend.global.domain.enums.PostAccess;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Diary extends BaseEntity {
+
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long postId;
+
+  // String?
+  private String postCategory;
+
+  private String postTitle;
+
+  private String postBody;
+
+  private PostAccess postAccess;
+
+  @ElementCollection
+  private List<String> keywords;
+  // 게시글 임시저장?
+
 }
