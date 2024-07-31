3070f7bf79b5f93e38fac3b2868cc41ed2ac5d02
ParkJh38 <harryest@naver.com>
2024-07-31 23:29:01 +0900

    #65 Feat: 키워드 입력을 통해 관련 카테고리 리스트 조회 기능 구현

--- src/main/java/com/codiary/backend/global/domain/entity/mapping/Categories.java
+++ src/main/java/com/codiary/backend/global/domain/entity/mapping/Categories.java
@@ -1,5 +1,6 @@
 package com.codiary.backend.global.domain.entity.mapping;
 
+import com.codiary.backend.global.domain.common.BaseEntity;
 import com.codiary.backend.global.domain.entity.Post;
 import com.codiary.backend.global.domain.entity.Member;
 import jakarta.persistence.*;
@@ -17,27 +18,14 @@ public class Categories {
   @Column(name = "category_id", nullable = false, columnDefinition = "bigint")
   private Long categoryId;
 
-  //@Enumerated(EnumType.STRING)
   @Column(name = "name", nullable = false, columnDefinition = "varchar(100)")
   private String name;
 
-//  @ManyToOne(fetch = FetchType.LAZY)
-//  @JoinColumn(name = "member_id")
-//  private Member member;
-
-//  @ManyToOne(fetch = FetchType.LAZY)
-//  @JoinColumn(name = "post_id")
-//  private Post post;
   @ManyToMany(mappedBy = "categoriesList")
   private List<Post> posts = new ArrayList<>();
 
   @Builder
   public Categories(String name) { this.name = name;}
-  public static Categories createCategory(Post post, String name, Member member) {
-    Categories category = new Categories(name);
-    category.getPosts().add(post);
-    post.getCategoriesList().add(category);
-    return category;
-  }
+
 
 }
--- src/main/java/com/codiary/backend/global/service/CategoryService/CategoryCommandServiceImpl.java
+++ src/main/java/com/codiary/backend/global/service/CategoryService/CategoryCommandServiceImpl.java
@@ -2,7 +2,8 @@ package com.codiary.backend.global.service.CategoryService;
 
 import com.codiary.backend.global.domain.entity.Post;
 import com.codiary.backend.global.domain.entity.mapping.Categories;
-import com.codiary.backend.global.repository.CategoriesRepository;
+import com.codiary.backend.global.repository.CategoryRepository;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -12,9 +13,10 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional
 @Slf4j
+@Getter
 public class CategoryCommandServiceImpl implements CategoryCommandService{
 
-    private final CategoriesRepository categoriesRepository;
+    private final CategoryRepository categoriesRepository;
 
     @Transactional
     @Override
