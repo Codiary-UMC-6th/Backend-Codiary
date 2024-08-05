a23b7941d17c879a0b6a51963aa09e0c66743f01
yumzen <yeom.ji.eun35@gmail.com>
2024-08-05 17:11:07 +0900

    Chore: cors 설정

--- src/main/java/com/codiary/backend/global/config/SecurityConfig.java
+++ src/main/java/com/codiary/backend/global/config/SecurityConfig.java
@@ -50,7 +50,8 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOriginPatterns(List.of("*"));
+        config.setAllowedOriginPatterns(List.of("*", "http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:3000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("*"));
