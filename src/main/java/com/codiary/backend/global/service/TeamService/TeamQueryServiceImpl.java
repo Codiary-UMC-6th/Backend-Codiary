3aa7190a4c67cea5b116878032297423ba718afe
insa658723 <insa658723@gmail.com>
2024-08-07 16:31:01 +0900

    #91 Feat: 팀 조회 서비스 구현체 작성

--- src/main/java/com/codiary/backend/global/service/TeamService/TeamQueryServiceImpl.java
+++ src/main/java/com/codiary/backend/global/service/TeamService/TeamQueryServiceImpl.java
@@ -1,4 +1,30 @@
 package com.codiary.backend.global.service.TeamService;
 
-public class TeamQueryServiceImpl {
+import com.codiary.backend.global.domain.entity.Team;
+import com.codiary.backend.global.repository.TeamRepository;
+import com.codiary.backend.global.web.dto.Team.TeamResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class TeamQueryServiceImpl implements TeamQueryService {
+
+  private final TeamRepository teamRepository;
+
+  @Override
+  public TeamResponseDTO.TeamCheckResponseDTO getTeamById(Long teamId) {
+    Team team = teamRepository.findById(teamId)
+        .orElseThrow(() -> new IllegalArgumentException("Invalid team ID"));
+
+    return TeamResponseDTO.TeamCheckResponseDTO.builder()
+        .teamId(team.getTeamId())
+        .name(team.getName())
+        .intro(team.getIntro())
+        .profilePhoto(team.getProfilePhoto())
+        .github(team.getGithub())
+        .email(team.getEmail())
+        .linkedIn(team.getLinkedin())
+        .build();
+  }
 }
