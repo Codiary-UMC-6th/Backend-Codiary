be84d1c5cc5ddbd0a76c301cdb49f44dda694fd0
insa658723 <insa658723@gmail.com>
2024-08-11 14:49:22 +0900

    #145 Feat: TeamFollowRepository 추가

---
+++ src/main/java/com/codiary/backend/global/repository/TeamFollowRepository.java
@@ -0,0 +1,15 @@
+package com.codiary.backend.global.repository;
+
+import com.codiary.backend.global.domain.entity.Member;
+import com.codiary.backend.global.domain.entity.Team;
+import com.codiary.backend.global.domain.entity.TeamFollow;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface TeamFollowRepository extends JpaRepository<TeamFollow, Long> {
+  @Query("SELECT tf FROM TeamFollow tf WHERE tf.member = :member AND tf.team = :team")
+  Optional<TeamFollow> findByMemberAndTeam(@Param("member") Member member, @Param("team") Team team);
+}
