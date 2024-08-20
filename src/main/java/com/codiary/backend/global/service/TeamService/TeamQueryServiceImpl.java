5d5c4d6fcda3e76c1b3ba9e1adb10f884cd74273
insa658723 <insa658723@gmail.com>
2024-08-20 15:49:15 +0900

    #187 Fix: 팀 팔로우 및 프로필 수정 권한 분리 로직 수정

--- src/main/java/com/codiary/backend/global/service/TeamService/TeamQueryServiceImpl.java
+++ src/main/java/com/codiary/backend/global/service/TeamService/TeamQueryServiceImpl.java
@@ -7,12 +7,11 @@ import com.codiary.backend.global.apiPayload.exception.GeneralException;
 import com.codiary.backend.global.domain.entity.Member;
 import com.codiary.backend.global.domain.entity.Team;
 import com.codiary.backend.global.domain.entity.TeamFollow;
-import com.codiary.backend.global.repository.TeamBannerImageRepository;
-import com.codiary.backend.global.repository.TeamFollowRepository;
-import com.codiary.backend.global.repository.TeamProfileImageRepository;
+import com.codiary.backend.global.domain.enums.MemberRole;
+import com.codiary.backend.global.jwt.SecurityUtil;
+import com.codiary.backend.global.repository.*;
 import com.codiary.backend.global.converter.TeamConverter;
 import com.codiary.backend.global.domain.entity.Team;
-import com.codiary.backend.global.repository.TeamRepository;
 import com.codiary.backend.global.web.dto.Team.TeamResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.Hibernate;
@@ -29,6 +28,7 @@ public class TeamQueryServiceImpl implements TeamQueryService {
   private final TeamRepository teamRepository;
   private final TeamFollowService teamFollowService;
   private final TeamFollowRepository teamFollowRepository;
+  private final MemberRepository memberRepository;
 
   @Override
   public ApiResponse<TeamResponseDTO.TeamImageDTO> getBannerImage(Long teamId) {
@@ -60,8 +60,22 @@ public class TeamQueryServiceImpl implements TeamQueryService {
     Team team = teamRepository.findById(teamId)
         .orElseThrow(() -> new IllegalArgumentException("Invalid team ID"));
 
+    Member currentMember = getRequester();
+
+    boolean isAdmin = team.getTeamMemberList().stream()
+        .anyMatch(teamMember ->
+            teamMember.getMember().getMemberId().equals(currentMember.getMemberId()) &&
+                teamMember.getTeamMemberRole() == MemberRole.ADMIN
+        );
+
     team.getTeamMemberList().size();
-    return TeamConverter.toTeamCheckResponseDTO(team);
+    return TeamConverter.toTeamCheckResponseDTO(team, isAdmin);
+  }
+
+  private Member getRequester() {
+    String userEmail = SecurityUtil.getCurrentMemberEmail(); // 현재 사용자의 이메일을 가져옴
+    return memberRepository.findByEmail(userEmail)
+        .orElseThrow(() -> new GeneralException(ErrorStatus.MEMBER_NOT_FOUND));
   }
 
   @Override
