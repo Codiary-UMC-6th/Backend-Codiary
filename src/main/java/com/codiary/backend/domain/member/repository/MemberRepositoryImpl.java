872e226cc9141bb2452994d1dc770454501998e7
yumzen <yeom.ji.eun35@gmail.com>
2024-09-30 13:34:23 +0900

    #227 Feat: Qclass static import로 변경

--- src/main/java/com/codiary/backend/domain/member/repository/MemberRepositoryImpl.java
+++ src/main/java/com/codiary/backend/domain/member/repository/MemberRepositoryImpl.java
@@ -2,10 +2,6 @@ package com.codiary.backend.domain.member.repository;
 
 import com.codiary.backend.domain.member.entity.Member;
 import com.codiary.backend.domain.member.entity.MemberProjectMap;
-import com.codiary.backend.domain.member.entity.QMemberProjectMap;
-import com.codiary.backend.domain.project.entity.QProject;
-import com.codiary.backend.domain.team.entity.QTeam;
-import com.codiary.backend.domain.team.entity.QTeamMember;
 import com.codiary.backend.domain.team.entity.TeamMember;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -15,9 +11,14 @@ import java.util.Optional;
 
 import static com.codiary.backend.domain.member.entity.QMember.member;
 import static com.codiary.backend.domain.techstack.entity.QTechStacks.techStacks;
+import static com.codiary.backend.domain.member.entity.QMemberProjectMap.memberProjectMap;
+import static com.codiary.backend.domain.project.entity.QProject.project;
+import static com.codiary.backend.domain.team.entity.QTeamMember.teamMember;
+import static com.codiary.backend.domain.team.entity.QTeam.team;
 
 @RequiredArgsConstructor
 public class MemberRepositoryImpl implements MemberRepositoryCustom {
+
     private final JPAQueryFactory queryFactory;
 
     public Optional<Member> findMemberWithTechStacksAndProjectsAndTeam(Long userId) {
@@ -28,17 +29,37 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
                 .fetchOne();
 
         fetchMemberProjects(userId, fetchedMember);
-
         fetchTeamMembers(userId, fetchedMember);
+
+        return Optional.ofNullable(fetchedMember);
+    }
+
+    public Optional<Member> findByIdWithAndFollowersAndFollowings(Long id) {
+        Member fetchedMember = queryFactory
+                .selectFrom(member)
+                .leftJoin(member.followers)
+                .leftJoin(member.followings)
+                .where(member.memberId.eq(id))
+                .fetchOne();
+
+        return Optional.ofNullable(fetchedMember);
+    }
+
+    public Optional<Member> findByIdWithFollowers(Long id) {
+        Member fetchedMember = queryFactory
+                .selectFrom(member)
+                .leftJoin(member.followers)
+                .where(member.memberId.eq(id))
+                .fetchOne();
 
         return Optional.ofNullable(fetchedMember);
     }
 
     private void fetchMemberProjects(Long userId, Member fetchedMember) {
         List<MemberProjectMap> projects = queryFactory
-                .selectFrom(QMemberProjectMap.memberProjectMap)
-                .leftJoin(QMemberProjectMap.memberProjectMap.project, QProject.project)
-                .where(QMemberProjectMap.memberProjectMap.member.memberId.eq(userId))
+                .selectFrom(memberProjectMap)
+                .leftJoin(memberProjectMap.project, project)
+                .where(memberProjectMap.member.memberId.eq(userId))
                 .fetch();
 
         fetchedMember.setMemberProjectMapList(projects);
@@ -46,9 +67,9 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
 
     private void fetchTeamMembers(Long userId, Member fetchedMember) {
         List<TeamMember> teamMembers = queryFactory
-                .selectFrom(QTeamMember.teamMember)
-                .leftJoin(QTeamMember.teamMember.team, QTeam.team)
-                .where(QTeamMember.teamMember.member.memberId.eq(userId))
+                .selectFrom(teamMember)
+                .leftJoin(teamMember.team, team)
+                .where(teamMember.member.memberId.eq(userId))
                 .fetch();
 
         fetchedMember.setTeamMemberList(teamMembers);
