3372f901f3bc9a9e05005e12af739fd3a9dd0ee6
lee-haeseung <lhser123@cau.ac.kr>
2024-10-14 19:33:38 +0900

    #240 Refactor: Service method 수정
    반환하는 자료형 수정
    validation -> business login -> response 순서로 흐름 수정
    필요한 경우 converter 제작

--- src/main/java/com/codiary/backend/domain/member/service/AuthService.java
+++ src/main/java/com/codiary/backend/domain/member/service/AuthService.java
@@ -5,9 +5,7 @@ import com.codiary.backend.domain.member.dto.request.MemberRequestDTO;
 import com.codiary.backend.domain.member.dto.response.MemberResponseDTO;
 import com.codiary.backend.domain.member.entity.Member;
 import com.codiary.backend.domain.member.repository.MemberRepository;
-import com.codiary.backend.global.apiPayload.ApiResponse;
 import com.codiary.backend.global.apiPayload.code.status.ErrorStatus;
-import com.codiary.backend.global.apiPayload.code.status.SuccessStatus;
 import com.codiary.backend.global.apiPayload.exception.handler.MemberHandler;
 import com.codiary.backend.global.jwt.JwtTokenProvider;
 import com.codiary.backend.global.jwt.TokenInfo;
@@ -40,7 +38,6 @@ public class AuthService {
         // - 이메일, 닉네임, 비밀번호 적합한지 확인
         // - 존재하는 닉네임/이메일인지 확인
         signUpRequest.isCorrect();
-
         if (memberRepository.existsByEmail(signUpRequest.email())
                 || memberRepository.existsByNickname(signUpRequest.nickname())) {
             throw new MemberHandler(ErrorStatus.MEMBER_ALREADY_EXISTS);
@@ -56,29 +53,39 @@ public class AuthService {
     }
 
     @Transactional
-    public ApiResponse<String> checkEmailDuplication(String email) {
-        if (memberRepository.existsByEmail(email)) {
-            throw new MemberHandler(ErrorStatus.MEMBER_EMAIL_ALREADY_EXISTS);
-        }
-
+    public String checkEmailDuplication(String email) {
+        // validation: 이메일 형식 확인
         if (!email.matches("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")) {
             throw new MemberHandler(ErrorStatus.MEMBER_WRONG_EMAIL);
         }
 
-        return ApiResponse.onSuccess(SuccessStatus.MEMBER_OK, "사용가능한 이메일입니다!");
+        // business logic: 이메일 중복 확인
+        if (memberRepository.existsByEmail(email)) {
+            throw new MemberHandler(ErrorStatus.MEMBER_EMAIL_ALREADY_EXISTS);
+        }
+
+        // response: 사용가능 여부 반환
+        return "사용가능한 이메일입니다!";
     }
 
     @Transactional
-    public ApiResponse<String> checkNicknameDuplication(String nickname) {
+    public String checkNicknameDuplication(String nickname) {
+        // validation: 닉네임 형식 확인
+        if (nickname.trim().isEmpty()) {
+            throw new MemberHandler(ErrorStatus.MEMBER_NAME_NOT_EXIST);
+        }
+
+        // business logic: 닉네임 중복 확인
         if (memberRepository.existsByNickname(nickname)) {
             throw new MemberHandler(ErrorStatus.MEMBER_NICKNAME_ALREADY_EXISTS);
         }
-        return ApiResponse.onSuccess(SuccessStatus.MEMBER_OK, "사용가능한 닉네임입니다!");
+
+        return "사용가능한 닉네임입니다!";
     }
 
 
     @Transactional
-    public ApiResponse<MemberResponseDTO.MemberTokenResponseDTO> login(MemberRequestDTO.MemberLoginRequestDTO loginRequest) {
+    public MemberResponseDTO.MemberTokenResponseDTO login(MemberRequestDTO.MemberLoginRequestDTO loginRequest) {
 
         try {
             // 1. Login ID/PW를 기반으로 Authentication 객체 생성
@@ -93,12 +100,12 @@ public class AuthService {
             Member getMember = memberRepository.findByEmail(loginRequest.email()).orElseThrow();
             TokenInfo tokenInfo = jwtTokenProvider.generateToken(authentication, getMember.getMemberId());
 
-            return ApiResponse.of(SuccessStatus.MEMBER_OK, MemberResponseDTO.MemberTokenResponseDTO.builder()
+            return MemberResponseDTO.MemberTokenResponseDTO.builder()
                     .email(getMember.getEmail())
                     .nickname(getMember.getNickname())
                     .tokenInfo(tokenInfo)
                     .memberId(getMember.getMemberId())
-                    .build());
+                    .build();
         } catch (AuthenticationException e) {
             throw new MemberHandler(ErrorStatus.MEMBER_LOGIN_FAIL);
         }
@@ -106,34 +113,38 @@ public class AuthService {
 
     @Transactional
     public String logout(String refreshToken) {
+        // validation: 로그인 상태인지 확인, 정상 토큰인지 확인
         if (redisTemplate.hasKey(refreshToken)) {
             throw new MemberHandler(ErrorStatus.MEMBER_ALREADY_LOGGED_OUT);
         }
-
-        if (jwtTokenProvider.validateToken(refreshToken)) {
-            Date expirationDate = jwtTokenProvider.getExpirationTimeFromToken(refreshToken);
-            long expirationTime = (expirationDate.getTime() - (new Date()).getTime()) / 1000;
-            redisTemplate.opsForValue().set(refreshToken, "blacklisted", expirationTime, TimeUnit.SECONDS);
-        } else {
+        if (!jwtTokenProvider.validateToken(refreshToken)) {
             throw new MemberHandler(ErrorStatus.MEMBER_WRONG_TOKEN);
         }
+
+        // business logic: 만료 날짜 설정 및 refresh token blacklist에 추가
+        Date expirationDate = jwtTokenProvider.getExpirationTimeFromToken(refreshToken);
+        long expirationTime = (expirationDate.getTime() - (new Date()).getTime()) / 1000;
+        redisTemplate.opsForValue().set(refreshToken, "blacklisted", expirationTime, TimeUnit.SECONDS);
+
         return "로그아웃되었습니다.";
     }
 
     @Transactional
     public MemberResponseDTO.TokenRefreshResponseDTO refresh(String refreshToken) {
+        // validation: 정상 토큰 확인 및 로그아웃한 유저인지 확인
         if (!jwtTokenProvider.validateToken(refreshToken)) {
             throw new MemberHandler(ErrorStatus.MEMBER_WRONG_TOKEN);
         }
-
         if (redisTemplate.hasKey(refreshToken)) {
             throw new MemberHandler(ErrorStatus.MEMBER_ALREADY_LOGGED_OUT);
         }
 
+        // business logic: access token 재할당
         String userEmail = jwtTokenProvider.getUserEmailFromToken(refreshToken);
         String newAccessToken = jwtTokenProvider.createAccessToken(userEmail);
         Member member = memberRepository.findByEmail(userEmail).orElseThrow(() -> new MemberHandler(ErrorStatus.MEMBER_NOT_FOUND));
 
+        // response: 만들어진 토큰 반환
         return MemberResponseDTO.TokenRefreshResponseDTO.builder()
                 .accessToken(newAccessToken)
                 .email(userEmail)
