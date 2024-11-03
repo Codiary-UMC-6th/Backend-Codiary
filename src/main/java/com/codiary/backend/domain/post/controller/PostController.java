ba9fd35afc44731ddf3dcfcbe4eb385f79e238cf
JH Park <harryest@naver.com>
2024-11-03 16:38:57 +0900

    Feat: 게시글 생성 API 기능 구현

--- src/main/java/com/codiary/backend/domain/post/controller/PostController.java
+++ src/main/java/com/codiary/backend/domain/post/controller/PostController.java
@@ -1,28 +1,69 @@
 package com.codiary.backend.domain.post.controller;
 
+import com.codiary.backend.domain.member.service.MemberCommandService;
 import com.codiary.backend.domain.post.converter.PostConverter;
 import com.codiary.backend.domain.post.dto.response.PostResponseDTO;
+import com.codiary.backend.domain.post.dto.request.PostRequestDTO;
 import com.codiary.backend.domain.post.entity.Post;
+import com.codiary.backend.domain.member.entity.Member;
+import com.codiary.backend.domain.post.service.PostCommandService;
 import com.codiary.backend.domain.post.service.PostService;
 import com.codiary.backend.global.apiPayload.ApiResponse;
 import com.codiary.backend.global.apiPayload.code.status.SuccessStatus;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.data.domain.Pageable;
+import com.codiary.backend.global.jwt.JwtTokenProvider;
+
 
 @RequiredArgsConstructor
+@Validated
 @RestController
 @RequestMapping("/api/v2/post")
+@CrossOrigin
+@Slf4j
 @Tag(name = "게시글 API", description = "게시글 관련 API입니다.")
 public class PostController {
     private final PostService postService;
+    private final PostCommandService postCommandService;
+    private final MemberCommandService memberCommandService;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    // 게시글 생성하기
+    @PostMapping(consumes = "multipart/form-data")
+    @Operation(summary = "게시글 생성 API", description = "게시글을 생성합니다. **카테고리 설정은 게시글 생성과는 별도로 설정해야 됩니다.**")
+    public ApiResponse<PostResponseDTO.CreatePostResultDTO> createPost(@ModelAttribute PostRequestDTO.CreatePostRequestDTO request) {
+        Member member = memberCommandService.getRequester();
+        jwtTokenProvider.isValidToken(member.getMemberId());
+
+        //Post newPost = postCommandService.createPost(request);
+        Post newPost = postCommandService.createPost(request);
+        return ApiResponse.onSuccess(SuccessStatus.POST_OK, PostConverter.toCreateResultDTO(newPost));
+    }
+
+    // 멤버의 게시글 수정하기
+
+
+    // 게시글 삭제하기
+
+
+
+
+
+
+
+
+
+
+
+
     @Operation(summary = "게시글 검색 결과 페이지네이션", description = "게시글(제목/내용) 키워드 검색 결과를 페이지네이션하여 반환합니다.")
     @GetMapping("/search")
     public ApiResponse<Page<PostResponseDTO.SimplePostResponseDTO>> searchPost(
--- src/main/java/com/codiary/backend/domain/post/converter/PostConverter.java
+++ src/main/java/com/codiary/backend/domain/post/converter/PostConverter.java
@@ -1,10 +1,19 @@
 package com.codiary.backend.domain.post.converter;
 
+import com.codiary.backend.domain.post.dto.request.PostRequestDTO;
 import com.codiary.backend.domain.post.dto.response.PostResponseDTO;
 import com.codiary.backend.domain.post.entity.Post;
+import com.codiary.backend.domain.post.enumerate.PostAccess;
+import com.codiary.backend.domain.project.entity.Project;
+import com.codiary.backend.domain.team.entity.Team;
 import org.springframework.data.domain.Page;
+import com.codiary.backend.domain.project.repository.ProjectRepository;
+import com.codiary.backend.domain.team.repository.TeamRepository;
+
+import java.util.stream.Collectors;
 
 public class PostConverter {
+
     public static Page<PostResponseDTO.SimplePostResponseDTO> toPostListResponseDto(Page<Post> postList) {
         return postList.map(PostConverter::toSimplePostResponseDto);
     }
@@ -19,4 +28,50 @@ public class PostConverter {
                 .thumbnailImage(post.getThumbnailImage() != null ? post.getThumbnailImage().getFileUrl() : null)
                 .build();
     }
+
+
+    public static Post toPost(PostRequestDTO.CreatePostRequestDTO request, TeamRepository teamRepository, ProjectRepository projectRepository) {
+        Team team = null;
+        Project project = null;
+        if (request.teamId() != null) {
+            team = teamRepository.findById(request.teamId()).orElseThrow(() -> new IllegalArgumentException("Team not found with id: " + request.teamId()));
+        }
+        if (request.projectId() != null) {
+            project = projectRepository.findById(request.projectId()).orElseThrow(() -> new IllegalArgumentException("Project not found with id: " + request.projectId()));
+        }
+        return Post.builder()
+                .postTitle(request.postTitle())
+                .postBody(request.postBody())
+                .team(team)
+                .project(project)
+                .postStatus(request.postStatus() != null ? request.postStatus() : true)  // 기본값 설정
+                .postAccess(request.postAccess() != null ? request.postAccess() : PostAccess.MEMBER)  // 기본값 설정
+                .build();
+    }
+
+
+
+    public static PostResponseDTO.CreatePostResultDTO toCreateResultDTO(Post post) {
+        return PostResponseDTO.CreatePostResultDTO.builder()
+                .postId(post.getPostId())
+                .memberId(post.getMember().getMemberId())
+                .teamId(post.getTeam() != null ? post.getTeam().getTeamId() : null)
+                .projectId(post.getProject() != null ? post.getProject().getProjectId() : null)
+                .postTitle(post.getPostTitle())
+                .postBody(post.getPostBody())
+                .postStatus(post.getPostStatus())
+                .coauthorIds(post.getAuthorsList().stream()
+                        .map(author -> author.getMember().getMemberId())
+                        .collect(Collectors.toSet()))
+                .postAccess(post.getPostAccess())
+                .thumbnailImageUrl((post.getThumbnailImage() != null)
+                        ? post.getThumbnailImage().getFileUrl()
+                        : "")
+                .postFileList(PostFileConverter.toPostFileListDTO(post.getPostFileList()))
+                .build();
+    }
+
+
+
+
 }
