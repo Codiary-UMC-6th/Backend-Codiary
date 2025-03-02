7542b1a4390d11914efc84ccd590582e2dbd208e
lee-haeseung <lhser123@cau.ac.kr>
2025-03-03 05:41:23 +0900

    feat: test용 api 생성

--- src/main/java/com/codiary/backend/domain/member/controller/MemberController.java
+++ src/main/java/com/codiary/backend/domain/member/controller/MemberController.java
@@ -41,7 +41,7 @@ public class MemberController {
     private final MemberCommandService memberCommandService;
     private final MemberQueryService memberQueryService;
 
-    @GetMapping("/test")
+    @GetMapping("/test2")
     public ApiResponse<String> test() {
         return ApiResponse.onSuccess(SuccessStatus.MEMBER_OK, "asdf");
     }
